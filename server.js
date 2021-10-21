5b9d086d807ccfefa08f0632771481f9ad662ea9
aidar <aidar0.1kaz@gmail.com>
2021-10-21 19:02:18 +0600

    initial2

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ require("./app/routes/comment.routes")(app);
 require("./app/routes/album.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8083;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
